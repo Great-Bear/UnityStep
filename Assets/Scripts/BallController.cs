beeb14a9f4be2a834f5c49030853122d77f5be51
Bogdan <bgroholsky@gmail.com>
2021-12-06 21:31:15 +0200

    init

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float forceFactor = 200;
+
+    private Vector3 forceDirection;
+    private Rigidbody rb;
+    void Start()
+    {
+        forceDirection = Vector3.zero;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forceDirection.x = Input.GetAxis("Horizontal");
+        forceDirection.z = Input.GetAxis("Vertical");
+        rb.AddForce(forceDirection * forceFactor * Time.deltaTime);
+    }
+}
